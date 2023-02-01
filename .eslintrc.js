161a008504f2a77fa767c67a14eae86bd40f947d
Trevor Blades <tdblades@gmail.com>
2023-02-01 15:19:34 -0800

    Add mdx linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,20 @@
-module.exports = {
+// @ts-check
+
+/**
+ * @type {import('eslint').ESLint.ConfigData}
+ **/
+const eslintConfig = {
   extends: [
     "plugin:@trevorblades/react",
     "plugin:@trevorblades/typescript",
     "plugin:prettier/recommended",
   ],
+  overrides: [
+    {
+      files: ["*.mdx"],
+      extends: ["plugin:mdx/recommended"],
+    },
+  ],
 };
+
+module.exports = eslintConfig;
